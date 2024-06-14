87ed14821a10c01d0e9916876cc96a8a3316c928
buonviyeuanh456@gmail.com <buonviyeuanh456@gmail.com>
2024-06-14 20:07:53 +0700

    upload source code

---
+++ config.js
@@ -0,0 +1,10 @@
+const nameGirl = 'Ní';
+const giftUrl = 'https://www.facebook.com/xuangiau914928';
+const eventName = 'Hello';
+const titleCard = 'Xuân Giàu';
+const contentCard = 'Xinnnn chàooo Xuân Giàu xinh gái, học giỏi, dễ thương nhất quả đất nhé :>';
+
+
+const giftImage = 'Xuan-giau1.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
